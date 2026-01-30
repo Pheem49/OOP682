d3a18a7cb1df07a86b5d90de9f6dd3d0fc603a08
Pheem49 <EMAIL_GITHUB@EXAMPLE.COM>
2026-01-30 15:43:52 +0700

    เพิ่มฟีเจอร์ User class

---
+++ student.py
@@ -0,0 +1,9 @@
+from person import Person
+
+class Student(Person):
+    def __init__(self, pid, name, age, student_id):
+        super().__init__(pid, name, age)
+        self.student_id = student_id
+
+    def __str__(self):
+        return f"Student[PID={self.pid}, Name={self.name}, Age={self.age}, StudentID={self.student_id}]"
